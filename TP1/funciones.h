1ac10ee8952ba26baba22878026f222ef0cfa1a3
rodoSolari <rodosolari033@gmail.com>
2020-04-04 02:42:32 -0300

    agregado descripcion de las funciones en funciones.h

--- TP1/funciones.h
+++ TP1/funciones.h
@@ -1,12 +1,22 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-
+//imprime el menu de la calculadora
 void imprimirMenu();
+
+//Devuelve la suma de 2 numeros
 int suma(int primerNumero, int segundoNumero);
+
+//Devuelve la resta de 2 numeros
 int resta(int primerNumero, int segundoNumero);
+
+//Devuelve la multiplicacion de 2 numeros
 int multiplicar(int primerNumero, int segundoNumero);
+
+//Devuelve la division de 2 numeros
 int dividir(int primerNumero, int segundoNumero);
+
+//devuelve el factorial del primer numero ingresado
 int calcularFactorial(int num);
 
 #endif // FUNCIONES_H_INCLUDED
