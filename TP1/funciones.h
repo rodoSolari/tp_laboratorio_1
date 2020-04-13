fc57dc7a58acb230bfb8b21966e878eb8bea4792
rodoSolari <rodosolari033@gmail.com>
2020-04-13 03:51:16 -0300

    modificado funciones realizarOperaciones e imprimirMenu

--- TP1/funciones.h
+++ TP1/funciones.h
@@ -2,7 +2,7 @@
 #define FUNCIONES_H_INCLUDED
 
 //imprime el menu de la calculadora
-void imprimirMenu();
+void imprimirMenu(int primerNumero,int segundoNumero);
 
 //Devuelve la suma de 2 numeros
 int suma(int primerNumero, int segundoNumero);
@@ -20,6 +20,6 @@ int dividir(int primerNumero, int segundoNumero);
 int calcularFactorial(int num);
 
 //realizar la operacion que elije el usuario con los 2 numeros ingresados
-void realizarOperacion(int opcion,int primerOperando, int segundoOperando);
+void realizarOperacion(int opcion, int primerNumero, int segundoNumero);
 
 #endif // FUNCIONES_H_INCLUDED
